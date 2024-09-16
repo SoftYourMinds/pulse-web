9e9d1c67d5e8c189ff7760d582e571341a4b6972
Daniil Shkarupa <d.shkarupa@boosteroid.com>
2024-09-16 12:52:56 +0300

    Add pipe

---
+++ src/app/shared/pipes/format-number.pipe.ts
@@ -0,0 +1,30 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+    name: 'formatNumber',
+    standalone: true,
+})
+export class FormatNumberPipe implements PipeTransform {
+    public transform(value: number): string | number {
+        if (value / 1_000_000 >= 10) 
+            return Math.floor(value / 1_000_000) + 'M';
+
+        if (value / 1_000_000 >= 1) 
+            return this.getRoundedToOneDecimalValueBasedOnNumber(value, 1_000_000) + 'M';
+
+        if (value / 1_000 >= 10) 
+            return Math.floor(value / 1_000) + 'K';
+        
+        if (value / 1_000 >= 1) 
+            return this.getRoundedToOneDecimalValueBasedOnNumber(value, 1_000) + 'K';
+
+        return value;
+    }
+
+    private getRoundedToOneDecimalValueBasedOnNumber(
+        value: number,
+        segmentationValue: number
+    ): number {
+        return Math.round((value * 10) / segmentationValue) / 10;
+    }
+}
