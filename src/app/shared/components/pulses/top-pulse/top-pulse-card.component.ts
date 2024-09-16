6c14ea7d27d06f719a5a3c4ac4455240adc4ade6
Daniil Shkarupa <d.shkarupa@boosteroid.com>
2024-09-16 12:52:48 +0300

    Add format numbers

--- src/app/shared/components/pulses/top-pulse/top-pulse-card.component.ts
+++ src/app/shared/components/pulses/top-pulse/top-pulse-card.component.ts
@@ -2,11 +2,12 @@ import { Component, Input } from '@angular/core';
 import { SvgIconComponent } from 'angular-svg-icon';
 import { LoadImgPathDirective } from '../../../directives/load-img-path/load-img-path.directive';
 import { IPulse } from '../../../interfaces';
+import { FormatNumberPipe } from '../../../pipes/format-number.pipe';
 
 @Component({
     selector: 'app-top-pulse-card',
     standalone: true,
-    imports: [LoadImgPathDirective, SvgIconComponent],
+    imports: [LoadImgPathDirective, SvgIconComponent, FormatNumberPipe],
     templateUrl: './top-pulse-card.component.html',
     styleUrl: './top-pulse-card.component.scss',
 })
