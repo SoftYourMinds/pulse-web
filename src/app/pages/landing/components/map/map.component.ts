1ffdadf89f5d024bb951781605314f3a2179bda5
Daniil Shkarupa <d.shkarupa@boosteroid.com>
2024-09-20 13:20:02 +0300

    Update map

--- src/app/pages/landing/components/map/map.component.ts
+++ src/app/pages/landing/components/map/map.component.ts
@@ -40,36 +40,57 @@ export class MapComponent implements OnInit {
 
     public ngOnInit(): void {
         // if (this.isPreview) {}
-
         this.h3Pulses$
             .pipe(takeUntilDestroyed(this.destroyed))
             .subscribe(this.addMarkersAndUpdateH3Polygons.bind(this));
     }
 
     public onMapLoad(map: mapboxgl.Map) {
-        console.log('[MAP]', map);
         this.map = map;
-        this.updateH3Pulses();
+
         this.addInitialLayersAndSourcesToDisplayData();
+        this.addH3PolygonsToMap();
+        this.updateH3Pulses();
     }
 
     public handleZoomEnd = () => this.updateH3Pulses();
-    
+
     public handleMoveEnd = () => this.updateH3Pulses();
 
     private addInitialLayersAndSourcesToDisplayData(): void {
+        const sourceId = 'h3-polygons';
+
         this.map.addSource('hexagons', {
             type: 'geojson',
         });
+
+        this.map.addSource(sourceId, {
+            type: 'geojson',
+            data: {
+                type: 'FeatureCollection',
+                features: [],
+            },
+        });
+
         this.map.addLayer({
-            id: 'hexagon-layer',
-            type: 'line',
+            id: 'hexagons',
+            type: 'fill',
             source: 'hexagons',
             layout: {},
             paint: {
-                'line-color': '#FFFFFF', // Border color
-                'line-width': 1.5, // Border width in pixels
-                // 'line-emissive-strength': 2
+                'fill-color': '#7700EE',
+                'fill-opacity': 0.15,
+            },
+        });
+
+        this.map.addLayer({
+            id: 'h3-polygons-layer',
+            type: 'line',
+            source: sourceId,
+            layout: {},
+            paint: {
+                'line-color': '#FFFFFF',
+                'line-width': 1.5,
             },
         });
     }
@@ -77,11 +98,14 @@ export class MapComponent implements OnInit {
     private addMarkersAndUpdateH3Polygons(h3PulsesData: any): void {
         const geojsonData: any = this.convertH3ToGeoJSON(h3PulsesData);
         (this.map.getSource('hexagons') as any).setData(geojsonData);
-
+        this.map.setPaintProperty('hexagons', 'fill-opacity', 0.15);
         this.addMarkersToMap(h3PulsesData);
     }
 
     private updateH3Pulses(): void {
+        this.map.setPaintProperty('hexagons', 'fill-opacity', 0);
+        this.addH3PolygonsToMap();
+
         const { _ne, _sw } = this.map.getBounds();
         const resolution = this.getResolutionBasedOnMapZoom();
 
@@ -93,15 +117,32 @@ export class MapComponent implements OnInit {
 
     private getResolutionBasedOnMapZoom(): number {
         const zoom = this.map.getZoom();
-        if (zoom < 4) return 1;
-        if (zoom < 11) return 2;
-        if (zoom < 14) return 3;
-        return 4;
+
+        const zoomResolutionMap: { [key: number]: number } = {
+            0: 1,
+            1: 1,
+            2: 1,
+            3: 1,
+            4: 2,
+            5: 2,
+            6: 3,
+            7: 4,
+            8: 4,
+            9: 5,
+            10: 7,
+            11: 7,
+            12: 8,
+            13: 9,
+            14: 10,
+            15: 11,
+        };
+
+        return zoomResolutionMap[Math.floor(zoom)] || 11;
     }
 
     private convertH3ToGeoJSON(data: any) {
         const features = Object.keys(data).map((h3Index) => {
-            const polygon = h3.cellToBoundary(h3Index, true);
+            const polygon = h3.h3ToGeoBoundary(h3Index, true);
             return {
                 type: 'Feature',
                 geometry: {
@@ -126,7 +167,7 @@ export class MapComponent implements OnInit {
     private addMarkersToMap(data: any): void {
         this.markers = [];
         Object.keys(data).forEach((h3Index: any) => {
-            const [lat, lng] = h3.cellToLatLng(h3Index);
+            const [lat, lng] = h3.h3ToGeo(h3Index);
             this.markers.push({
                 lng,
                 lat,
@@ -134,5 +175,72 @@ export class MapComponent implements OnInit {
                 h3Index,
             });
         });
+    }
+
+    private addH3PolygonsToMap(): void {
+        const bounds = this.map.getBounds();
+        const northWest = bounds.getNorthWest();
+        const southEast = bounds.getSouthEast();
+        const resolution = this.getResolutionBasedOnMapZoom();
+
+        const hexagons = this.getHexagonsForBounds(
+            northWest,
+            southEast,
+            resolution
+        );
+
+        const hexagonFeatures = hexagons.map((hex) =>
+            this.h3ToPolygonFeature(hex)
+        );
+
+        const sourceId = 'h3-polygons';
+
+        const source = this.map.getSource(sourceId) as mapboxgl.GeoJSONSource;
+
+        source.setData({
+            type: 'FeatureCollection',
+            features: hexagonFeatures,
+        });
+    }
+
+    private getStepBasedOnZoom(): number {
+        const zoom = this.map.getZoom();
+        if (zoom < 4) return 1;
+        if (zoom < 5) return 0.5;
+        if (zoom < 7) return 0.15;
+        if (zoom < 9) return 0.02;
+        if (zoom < 12) return 0.005;
+        if (zoom < 15) return 0.001;
+        return 0.0001;
+    }
+
+    private getHexagonsForBounds(
+        northWest: mapboxgl.LngLat,
+        southEast: mapboxgl.LngLat,
+        resolution: number
+    ): string[] {
+        const hexagons = [];
+        const step = this.getStepBasedOnZoom();
+
+        for (let lat = southEast.lat; lat < northWest.lat; lat += step) {
+            for (let lng = northWest.lng; lng < southEast.lng; lng += step) {
+                const hex = h3.geoToH3(lat, lng, resolution);
+                hexagons.push(hex);
+            }
+        }
+
+        return [...new Set(hexagons)]; // Remove duplicate hexagons
+    }
+
+    private h3ToPolygonFeature(hex: string): GeoJSON.Feature<GeoJSON.Polygon> {
+        const boundary = h3.h3ToGeoBoundary(hex, true);
+        return {
+            type: 'Feature',
+            geometry: {
+                type: 'Polygon',
+                coordinates: [boundary],
+            },
+            properties: {},
+        };
     }
 }
