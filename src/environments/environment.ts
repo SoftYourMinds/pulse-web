57735c675d73696b0ca6e06b61b67e9d1bef324f
Daniil Shkarupa <d.shkarupa@boosteroid.com>
2024-10-07 11:06:56 +0300

    Update apiurl

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: true,
-    apiUrl: 'http://pulse-dev-api.eastus.azurecontainer.io:8080',
+    apiUrl: 'https://pulse-dev-api.eastus.azurecontainer.io:8080',
     mapboxToken:
         'pk.eyJ1IjoidmliZXNwb3QiLCJhIjoiY2p4YnN5cHY3MDB3NDN4czVrOWtpanU4aCJ9.nexbisROPDgIPDPUPm5tvQ',
     mapStyleUrl:
