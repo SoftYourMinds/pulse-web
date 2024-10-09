247ca4f354f5ba2ded0ef59c6cff5c5fb15b91e4
Maxim Barishov <maximbarishov06@gmail.com>
2024-10-09 11:12:58 +0300

    fix 1px transperant line with header on map page

--- src/app/pages/landing/landing.component.ts
+++ src/app/pages/landing/landing.component.ts
@@ -1,7 +1,4 @@
 import { Component } from '@angular/core';
-import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
-import { filter } from 'rxjs';
-import { AppRoutes } from '../../shared/enums/app-routes.enum';
 
 @Component({
     selector: 'app-landing',
