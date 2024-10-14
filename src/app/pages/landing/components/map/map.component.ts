edc451a245f8c7af05c959409b43bd7153d63a95
shkarsmode <zshkarrr@gmail.com>
2024-10-14 14:19:49 +0300

    Updated logic of showing top topics and collisions

--- src/app/pages/landing/components/map/map.component.ts
+++ src/app/pages/landing/components/map/map.component.ts
@@ -31,6 +31,7 @@ export class MapComponent implements OnInit {
     public markers: any = [];
     public readonly mapboxStylesUrl: string = inject(MAPBOX_STYLE);
     public center: [number, number] = [-100.661, 37.7749];
+    public heatmapIntensity: number = 0.1;
 
     public map: mapboxgl.Map;
     public isToShowH3: boolean = true;
@@ -212,7 +213,6 @@ export class MapComponent implements OnInit {
             });
     }
 
-    public heatmapIntensity: number = 0.1;
     private subscribeOnDataListHeatmap(): void {
         this.heatMapData$
             .pipe(takeUntilDestroyed(this.destroyed))
